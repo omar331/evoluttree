30bd4b397516383586ccd47187c83415943fca0b
Your Name <omar331@gmail.com>
2016-06-23 18:30:11 -0300

    Estrutura básico

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return <div>
+                    <h3>Produto ISO 9001 - Conteudos</h3>
+                    <ul>
+                        <li>a ..... xxxxx </li>
+                        <li>b</li>
+                        <li>cssssxxxxxxxqqq</li>
+                        <li>a ..... xxxxx </li>
+                        <li>b</li>
+                        <li>cssssxxxxxxxqqq</li>
+                        <li>d ------</li>
+                        <li>e</li>
+                        <li>f lalalalala</li>
+                        <li>g qqqqqq wwq aaaaaaaaawww--------xxxxxxx</li>
+                        <li>g qqqqqq wwq sss aaaaaaaaawww--------xxxxxxx</li>
+                    </ul>
+
+                    <h2>Subtitulo do produto</h2>
+                    <h2>Titulo 2 xxxxxxxxxxx</h2>
+                </div>
+    }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        path: './',
+        filename: 'index.js'
+    },
+    devServer: {
+        inline: true,
+        port: 7000,
+        host: "0.0.0.0",
+        watchOptions: {
+            poll: true
+        }
+
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js$/,
+                exclude: /react-hot|node_modules/,
+                loaders: ['react-hot','babel?presets[]=es2015,presets[]=react']
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
