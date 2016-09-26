33ffb2d49d0bbae02a7046b8342af0aac0b563b9
Your Name <omar331@gmail.com>
2016-09-26 11:14:36 -0300

    Introducicing onMovePageConfirm for moving pages

--- src/components/elements/pages-list.tsx
+++ src/components/elements/pages-list.tsx
@@ -26,10 +26,41 @@ export default class PagesList extends React.Component<PagesProps, {}> {
     constructor(props:any) {
         super(props);
     }
+    handleMovePage(...args) {
+        const { depth, onMovePage, customComponents } = this.props
+
+        // it's possible to set an external confirmation handler.
+        // If so, it's called at the first level...
+        if ( depth == 0 ) {
+            if ( customComponents.onMovePageConfirm ) {
+                customComponents.onMovePageConfirm( onMovePage, ...args )
+                return
+            }
+        }
+
+        onMovePage(...args)
+    }
+
+
+    handleQuickLevelMove(...args) {
+        const { depth, onQuickLevelMove, customComponents } = this.props
+
+        // it's possible to set an external confirmation handler.
+        // If so, it's called at the first level...
+        if ( depth == 0 ) {
+            if ( customComponents.onMovePageConfirm ) {
+                customComponents.onMovePageConfirm( onQuickLevelMove, ...args )
+                return
+            }
+        }
+
+        onQuickLevelMove(...args)
+    }
+
     render() {
-        const { pages, generalInfo, parentPage, onTitleChange,
-                onNewPage, onMovePage, onChangeTreeState,
-                onQuickLevelMove, onChangePageInfo, onDeletePage,
+        const { pages, parentPage, onTitleChange,
+                onNewPage, onChangeTreeState,
+                onChangePageInfo, onDeletePage,
                 onStartEditPageBody,
                 depth, customComponents,
                 onPageItemBeginDrag, onPageItemEndDrag
@@ -39,7 +70,6 @@ export default class PagesList extends React.Component<PagesProps, {}> {
 
         let previousPage:any = null
         let pageComponentRes:any
-
 
         return(
             <div>
@@ -55,9 +85,9 @@ export default class PagesList extends React.Component<PagesProps, {}> {
                                                       pageOrder={order}
                                                       onTitleChange={onTitleChange}
                                                       onNewPage={onNewPage}
-                                                      onMovePage={onMovePage}
+                                                      onMovePage={this.handleMovePage.bind(this)}
                                                       onChangeTreeState={onChangeTreeState}
-                                                      onQuickLevelMove={onQuickLevelMove}
+                                                      onQuickLevelMove={this.handleQuickLevelMove.bind(this)}
                                                       onChangePageInfo={onChangePageInfo}
                                                       onDeletePage={onDeletePage}
                                                       onBeginDrag={onPageItemBeginDrag}
--- src/main_DEV.tsx
+++ src/main_DEV.tsx
@@ -7,10 +7,21 @@ declare var evltree_config: any
 declare var evltree_editing_product: any
 
 
+let custom_components = {
+    // ---> this handler may be used for asking confirmation before moving a page
+    //      it's triggered at QLMs as well
+    onMovePageConfirm(onConfirmCallback, ...onConfirmArgs) {
+        console.log(' ----> onConfirmCallback = %o    onConfirmArgs = %o   ', onConfirmCallback, onConfirmArgs )
+        onConfirmCallback(...onConfirmArgs)
+    }
+}
+
+
 ReactDOM.render(
     (
         <Evoluttree config={evltree_config}
                     editingProduct={evltree_editing_product}
+                    customComponents={custom_components}
         />
     ),
     document.getElementById('content')
